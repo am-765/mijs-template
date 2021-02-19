c67238ece34592c80e130f167029edcd85186b01
genui <42035804+genui@users.noreply.github.com>
2021-02-19 12:59:14 +0900

    nextsampleディレクトリ
    nextで作成したサンプルディレクトリを追加しました

---
+++ sample-next/component/bicycle.tsx
@@ -0,0 +1,29 @@
+export default function Bicycle (){
+    return(
+        <div>
+            <section id="bicycle" className="wrapper">
+            <h2 className="section-title">Bicycle</h2>
+            <ul>
+                <li>
+                    <img src="./bicycle1.jpg" alt="テキストテキストテキスト"></img>
+                    <h3 className="content-title">タイトルタイトル</h3>
+                    <p>テキストテキストテキスト</p>
+                </li>
+                <li>
+                    <img src="./bicycle2.jpg" alt="テキストテキストテキスト"></img>
+                    <h3 className="content-title">タイトルタイトル</h3>
+                    <p>テキストテキストテキスト</p>
+                </li>
+                <li>
+                    <img src="./bicycle3.jpg" alt="テキストテキストテキスト"></img>
+                    <h3 className="content-title">タイトルタイトル</h3>
+                    <p>テキストテキストテキスト</p>
+                </li>
+            </ul>
+                
+
+            
+            </section>
+        </div>
+    )
+}
---
+++ sample-next/component/main.tsx
@@ -0,0 +1,23 @@
+export default function main () {
+    return(
+        <div>
+            <div id="mainvisual">
+                <img src="./mainvisual.jpg" alt="テキストテキストテキスト"></img>
+            </div>
+            <section id="about" className="wrapper">
+                <h2 className="section-title">About</h2>
+                <div className="content">
+                    <img src="./about.jpg" alt="テキストテキストテキスト"></img>
+                    <div className="text">
+                        <h3 className="content-title">KAKERU MIYAICHI</h3>
+                        <p>
+                        テキストテキストテキストテキストテキストテキストテキスト
+                        テキストテキストテキストテキストテキストテキストテキスト
+                        テキストテキストテキストテキストテキストテキストテキスト
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
